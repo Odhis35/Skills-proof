32782316dfb71b1e5b671e7bbad77fb0a14f05bb
Odhis35 <donodhiambo.93@gmail.com>
2026-03-01 00:45:00 +0300

    Add index.css

---
+++ index.js
@@ -0,0 +1,20 @@
+let myList = []
+const EnterEL = document.getElementById("enter-EL")
+const EnterBtn = document.getElementById("enter-btn")
+const UlEl = document.getElementById("ul-el")
+
+EnterBtn.addEventListener("click", function()
+{
+    myList.push(EnterEL.value)
+    myFunction()
+    EnterEL.value = " "
+})
+
+function myFunction(){
+    let listItems = " "
+    for(i=0; i<myList.length; i++){
+        listItems += "<li>" + myList[i] + "</li>" 
+    }
+    UlEl.innerHTML = listItems
+}
+
